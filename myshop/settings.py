7e44eaa40ea33d4eb736b0b8d9b4957cee2008f1
egorpusto <egor_pusto@icloud.com>
2025-09-28 00:32:35 +0300

    Add shopping cart feature
    - Cart management system
    
    - Add to cart functionality

--- myshop/settings.py
+++ myshop/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'cart.apps.CartConfig',
     'shop.apps.ShopConfig',
 ]
 
@@ -122,5 +123,8 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+# Media settings
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+
+CART_SESSION_ID = 'cart'
