bf763ae1234d32fc5e1a740bc66753c1cc8356cb
egorpusto <egor_pusto@icloud.com>
2025-10-01 16:21:13 +0300

    feat(orders): Implement asynchronous email notifications with Celery

--- orders/views.py
+++ orders/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from cart.cart import Cart
 from .forms import OrderCreateForm
 from .models import OrderItem
+from .tasks import order_created
 
 def order_create(request):
     cart = Cart(request)
@@ -17,6 +18,7 @@ def order_create(request):
                     quantity=item['quantity']
                 )
             cart.clear()
+            order_created.delay(order.id)
             return render(
                 request, 'orders/order/created.html', {'order': order}
             )
