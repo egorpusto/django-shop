055cfc778fa6e20d0827bc6f0bd45d396f8454cd
Egor Anoshin <egor_pusto@icloud.com>
2026-03-09 15:23:30 +0300

    feat: add django-debug-toolbar for development

--- myshop/settings.py
+++ myshop/settings.py
@@ -183,3 +183,10 @@ LOGGING = {
         "payment": {"handlers": ["console"], "level": "WARNING", "propagate": False},
     },
 }
+
+# ─── Debug Toolbar ────────────────────────────────────────────────────────────
+
+if DEBUG:
+    INSTALLED_APPS += ["debug_toolbar"]
+    MIDDLEWARE.insert(0, "debug_toolbar.middleware.DebugToolbarMiddleware")
+    INTERNAL_IPS = ["127.0.0.1"]
